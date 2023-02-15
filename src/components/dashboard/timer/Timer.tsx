e9511997c299227cf198caa39fa9b2a4d007c22d
Alankazamm <bruno.alanc61@gmail.com>
2023-02-15 03:12:22 -0300

    custom timer hokk working todo some improvs in it

--- src/components/dashboard/timer/Timer.tsx
+++ src/components/dashboard/timer/Timer.tsx
@@ -7,78 +7,8 @@ import { useTimer } from "../../../custom-hooks/customTimerHook";
 import { TimerContainer } from "./TimerContainer";
 
 
-// Description: this custom hook is used to get the time and date
-//hooks
-import { useEffect, useState } from "react";
-//variables
-// types
-type Time = {
-    month: string;
-    day: string;
-    year: number;
-    hoursMin: string;
-};
-
 const Timer = () => {
-	const [time, setTime] = useState<Time>({} as Time);
-	console.log(time);
-	const [firstRender, setFirstRender] = useState(true);
-	if (firstRender === true) {
-		console.log("first render");
-           setFirstRender(false);
-		const date = new Date();
-		const day =
-			date.getDate() +
-			(date.getDate() % 10 === 1 && date.getDate() !== 11
-				? "st"
-				: date.getDate() % 10 === 2 && date.getDate() !== 12
-				? "nd"
-				: date.getDate() % 10 === 3 && date.getDate() !== 13
-				? "rd"
-				: "th");
-		const month = date.toLocaleString("en-Us", { month: "long" });
-		const year = date.getFullYear();
-		let hoursMin =
-			(date.getHours() < 10 ? "0" : "") +
-			date.getHours() +
-			":" +
-			((date.getMinutes() < 10 ? "0" : "") + date.getMinutes());
-
-		setTime({ month, day, year, hoursMin });
-		
-	}
-
-	
-	useEffect(() => {
-	
-		const timer = setInterval(() => {
-			const date = new Date();
-			const day =
-				date.getDate() +
-				(date.getDate() % 10 === 1 && date.getDate() !== 11
-					? "st"
-					: date.getDate() % 10 === 2 && date.getDate() !== 12
-					? "nd"
-					: date.getDate() % 10 === 3 && date.getDate() !== 13
-					? "rd"
-					: "th");
-			const month = date.toLocaleString("en-Us", { month: "long" });
-			const year = date.getFullYear();
-			let hoursMin =
-				(date.getHours() < 10 ? "0" : "") +
-				date.getHours() +
-				":" +
-				((date.getMinutes() < 10 ? "0" : "") + date.getMinutes());
-
-			setTime({ month, day, year, hoursMin });
-			//time every 10 seconds
-		}, 10000 );
-
-		return () => {
-			clearInterval(timer);
-		
-	}
-	}, [time]);
+	const time = useTimer();
 	
 	return (
 		<TimerContainer>
