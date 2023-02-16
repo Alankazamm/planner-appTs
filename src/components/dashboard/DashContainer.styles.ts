ca235cfab93043e858edc9e94690a474798f7b8b
Alankazamm <bruno.alanc61@gmail.com>
2023-02-16 03:15:54 -0300

    div styles improv fixed overflow bug

--- src/components/dashboard/DashContainer.styles.ts
+++ src/components/dashboard/DashContainer.styles.ts
@@ -2,8 +2,7 @@ import styled from 'styled-components';
 import globe from '/src/assets/svg/globe.svg';
 
 export const DashContainer = styled.div`
-position: relative;
-    
+    position: relative;
     background: rgba(255, 255, 255, 0.95);
     box-shadow: 4px 4px 70px 0px #00000040;
     min-height: 100vh;
