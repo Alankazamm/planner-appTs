eb3b72695b2626c9c4b958e8c607010c87a8f50e
Alankazamm <bruno.alanc61@gmail.com>
2023-04-04 15:47:17 -0300

    refactor(clean code): :closed_lock_with_key: updated api key secret

--- src/custom-hooks/customWeatherHook.ts
+++ src/custom-hooks/customWeatherHook.ts
@@ -9,7 +9,7 @@ export interface Forecast {
   
 };
 
-
+const API_KEY = import.meta.env.VITE_REACT_APP_WEATHER_API_KEY;
 export const useFetchWeather = (city: string): any => {
   //hooks
   const [forecast, setForecast] = useState<Forecast | unknown>();
@@ -18,7 +18,7 @@ export const useFetchWeather = (city: string): any => {
   useEffect(() => {
     if (firstTime === true) {
       try {
-        fetch(`https://api.weatherapi.com/v1/current.json?key=96292cdb0bbf468f931192735231102&q=${city}&lang=en`)
+        fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&lang=en`)
           .then((response) => response.status === 200 ? response.json() : console.log(response))
           .then(data => {
             console.log(data);
@@ -33,7 +33,7 @@ export const useFetchWeather = (city: string): any => {
       const weatherUpdate = setInterval(() => {
 
         try {
-          fetch(`https://api.weatherapi.com/v1/current.json?key=3274f2378a4045fc97835913232201&q=${city}&lang=en` )
+          fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&lang=en` )
             .then((response) => response.status === 200 ? response.json() : console.log(response.status))
             .then(data => {
               setForecast(data);
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from "./App";
 
 
 
+
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 		
 	<App />
