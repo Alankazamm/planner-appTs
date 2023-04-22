f060ecb384997c45be431148ca0a95bda1c7d25e
Alan Bruno <bruno.alanc61@gmail.com>
2023-04-22 17:34:05 +0000

    changing imports names

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 import { BrowserRouter } from "react-router-dom";
 
-import { UserContextProvider } from "./contexts/UserContext";
+import { UserContextProvider } from "./contexts/userContext";
 
 import { AppRoutes } from "./routes/AppRoutes";
 import { useContext, useEffect } from "react";
